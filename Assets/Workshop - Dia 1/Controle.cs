84f2f3c5cb1c4e3db838db702d63c96b2f0de61b
Mateus Marochi Olenik <mateusmarochi@hotmail.com>
2023-09-27 09:16:53 -0300

    New

---
+++ Assets/Workshop - Dia 1/Controle.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controle : MonoBehaviour
+{
+
+    Animator animator;
+
+     void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning =  animator.GetBool("isRunning");
+
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if(!isWalking && forwardPressed){
+            animator.SetBool("isWalking",true);
+        }
+        if(isWalking && !forwardPressed){
+            animator.SetBool("isWalking",false);
+        }
+        if(!isRunning && forwardPressed && runPressed){
+            animator.SetBool("isRunning",true);
+        }
+        if(isRunning && (!forwardPressed || !runPressed)){
+            animator.SetBool("isRunning",false);
+        }
+    }
+}
